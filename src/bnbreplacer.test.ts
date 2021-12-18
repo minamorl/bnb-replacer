0930a61b9b993d2164c017a590dce8f56ef25ee2
minamorl <minamorl@users.noreply.github.com>
2021-12-18 14:19:59 +0900

    Works

--- src/bnbreplacer.test.ts
+++ src/bnbreplacer.test.ts
@@ -1,6 +1,11 @@
 import * as bnb from "bread-n-butter"
 import { Replacer } from "./bnbreplacer"
 test("replacable", () => {
-  const result = Replacer.define(bnb.text("{").next(bnb.match(/[a-z]/).skip(bnb.text("}")))).evaluate("{a}{b}{c}{d}{e}")
-  expect(result.join("")).toBe("abcde")
+  const result = new Replacer(bnb.text("{").next(bnb.match(/[a-z]/).skip(bnb.text("}")))).evaluate("{a}{b}{c}{d}{e}")
+  expect(result).toBe("abcde")
+})
+
+test("replacable with non-replaced text", () => {
+  const result = new Replacer(bnb.text("{").next(bnb.match(/[a-z]/).skip(bnb.text("}")))).evaluate("{a}{b}{c}{d}{e}aaaaa{f}")
+  expect(result).toBe("abcdeaaaaaf")
 })
--- src/bnbreplacer.ts
+++ src/bnbreplacer.ts
@@ -1,24 +1,48 @@
 import * as bnb from "bread-n-butter"
 
-export class Replacer<A> {
-  private constructor(private parser: bnb.Parser<A>) {
-  }
-  static define(parser: bnb.Parser<unknown>): Replacer<unknown> {
+
+export type OKType<A> ={
+    start: bnb.SourceLocation;
+    value: A;
+    end: bnb.SourceLocation;
+}
+
+export class Replacer {
+  _internalParser: bnb.Parser<OKType<string>[]>
+  constructor(private parser: bnb.Parser<string>) {
+    const locationParser = bnb.all(bnb.location, parser, bnb.location).map(([start, value, end]) => ({ start, value, end }))
     const def = new bnb.Parser(ctx => {
       let i = 0
-      const result = parser.parse(ctx.input)
+      const result = locationParser.parse(ctx.input)
       const results = [result]
       while (i < ctx.input.length) {
         i++;
         for (let j = 0; j < i; j++) {
-          results.push(parser.parse(ctx.input.substring(j, i)))
+          const r = locationParser.parse(ctx.input.substring(j, i))
+          // Shift start index to the actual location
+          r.type === "ParseOK" && (r.value.start.index += j) && (r.value.end.index += j)
+          results.push(r)
         }
       }
-      return ctx.ok(ctx.input.length, results.filter(v => v.type === "ParseOK").map((v: any) => v.value))
+      return ctx.ok(
+        ctx.input.length,
+        (results.filter(v => v.type === "ParseOK") as unknown as bnb.ParseOK<OKType<string>>[]).map(v => v.value)
+      )
     })
-    return new Replacer(def)
+    this._internalParser = def
   }
   evaluate(input: string): any {
-    return (this.parser.tryParse(input) as any)
+    const parsed = this._internalParser.tryParse(input)
+    let result = input
+    let offset = 0
+    for (const p of parsed) {
+      result = this.replaceBetween({start: p.start.index + offset, end: p.end.index + offset, value: p.value, str: result})
+      offset -= p.end.index - p.start.index - p.value.length
+    }
+    return result
+  }
+  private replaceBetween(data: {start: number, end: number, str: string, value: string}) {
+    const r = data.str.substring(0, data.start) + data.value + data.str.substring(data.value.length + data.end - 1)
+    return r
   }
 }
